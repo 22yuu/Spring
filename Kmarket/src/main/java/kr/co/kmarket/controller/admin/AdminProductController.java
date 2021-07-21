b790a289cb7afdecb5d8d53c7987b85c6dfaa596
22yuu <22yuu@naver.com>
2021-07-21 11:18:47 +0900

    2021/07/21 Spring Boot Admin 페이지 상품 카테고리 기능 구현

--- Kmarket/src/main/java/kr/co/kmarket/controller/admin/AdminProductController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/admin/AdminProductController.java
@@ -1,11 +1,22 @@
 package kr.co.kmarket.controller.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.kmarket.service.admin.AdminProductService;
+import kr.co.kmarket.vo.Cate1Vo;
+import kr.co.kmarket.vo.Cate2Vo;
 
 @Controller
 public class AdminProductController {
 
+	@Autowired
+	private AdminProductService service;
+	
 	@GetMapping("/admin/product/list")
 	public String list() {
 		return "/admin/product/list";
@@ -15,4 +26,21 @@ public class AdminProductController {
 	public String register() {
 		return "/admin/product/register";
 	}
+	
+	// ResponseBody 어노테이션을 사용하면 Json데이터로 리턴해줌
+	@ResponseBody  
+	@GetMapping("/admin/product/getCate1")
+	public List<Cate1Vo> getCate1() {
+		List<Cate1Vo> cate1s = service.selectCate1();
+		return cate1s;
+	}
+	
+	@ResponseBody  
+	@GetMapping("/admin/product/getCate2")
+	public List<Cate2Vo> getCate2(int cate1) {
+		List<Cate2Vo> cate2s = service.selectCate2(cate1);
+		return cate2s;
+	}
+	
+	
 }
--- Kmarket/src/main/java/kr/co/kmarket/service/admin/AdminProductService.java
+++ Kmarket/src/main/java/kr/co/kmarket/service/admin/AdminProductService.java
@@ -1,9 +1,47 @@
 package kr.co.kmarket.service.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.kmarket.dao.admin.AdminProductDao;
+import kr.co.kmarket.vo.Cate1Vo;
+import kr.co.kmarket.vo.Cate2Vo;
+import kr.co.kmarket.vo.ProductVo;
+
+@Service
 public class AdminProductService {
+	
+	@Autowired
+	private AdminProductDao dao;
 
-	public AdminProductService() {
-		// TODO Auto-generated constructor stub
+	public void insertProduct(ProductVo VO) {
+		
+	};
+	
+	public ProductVo selectProduct() {
+		return null;
+	};
+	
+	public List<ProductVo> selectProducts(){
+		return null;
+	};
+	
+	public void updateProduct(ProductVo vo) {
+		
+	};
+	
+	public void deleteProduct(ProductVo vo) {
+		
+	};
+	
+	// 1차 카테고리 가져오기
+	public List<Cate1Vo> selectCate1(){
+		return dao.selectCate1();
+	};
+	
+	public List<Cate2Vo> selectCate2(int cate1) {
+		return dao.selectCate2(cate1);
 	}
-
 }
